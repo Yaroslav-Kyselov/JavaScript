760b17d39199d930548613ad77d583975fa18ff1
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-04-21 20:41:51 +0300

    js HW!

---
+++ Lesson1/HomeWork/HW1/main.js
@@ -0,0 +1,44 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let a='hello'
+let b='owu'
+let c='com';
+let d='ua'
+let e=1
+let f=10;
+let g=-999;
+let h=123;
+let i=3.14;
+let j=2.7;
+let k=16;
+let l=true;
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(i)
+console.log(j)
+console.log(k)
+console.log(l)
+
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ.
+//     З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName='Ярослав'
+let middleName='Олександрович'
+let lastName='Кисельов'
+let msg=firstName+' '+middleName+' '+lastName
+console.log(msg)
+
+// #4N0y5tufA
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+let q = 100;
+let w = '100';
+let s = true;
+typeof a
