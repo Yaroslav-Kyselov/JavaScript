3ef8dfaececdda23d3839a5538351f420641ff48
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-04-26 15:53:55 +0300

    js HW!

---
+++ Lesson/HomeWork/HW-2/main.js
@@ -0,0 +1,26 @@
+let courses = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (courses[0].monthDuration >5) {
+    console.log('super');
+}
+if (courses[1].monthDuration >5) {
+    console.log('super');
+}
+if (courses[2].monthDuration >5) {
+    console.log('super');
+}
+if (courses[3].monthDuration >5) {
+    console.log('super');
+}
+if (courses[4].monthDuration >5) {
+    console.log('super');
+}
+if (courses[5].monthDuration >5) {
+    console.log('super');
+}
