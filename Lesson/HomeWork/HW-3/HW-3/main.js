cf28fb0751559cebd79567225944fac581d68418
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-04-29 21:50:32 +0300

    js HW3 js + css

--- Lesson/HomeWork/HW-3/HW-3/main.js
+++ Lesson/HomeWork/HW-3/HW-3/main.js
@@ -274,25 +274,16 @@ let usersList = [
 for (const user of usersList) {
     document.write(
         `<div class=”users-box”>
-
-<div class=”user-block”>
-
-<h2>${user.id} – ${user.name} – ${user.username} </h2>
-
-    <h3>${user.email} – ${user.phone}</h3>
-
-    <div class=”address-block”>
-
-    <p>City – ${user.address.city}</p>
-
-    <p>Street – ${user.address.street}</p>
-
-    <p>Suite – ${user.address.suite}</p>
-
-    <p>Zip code – ${user.address.zipcode}</p>
-
-</div>
-
-</div>`
+            <div class=”user-block”>
+                <h2>${user.id} – ${user.name} – ${user.username} </h2>
+                <h3>${user.email} – ${user.phone}</h3>
+            </div>    
+            <div class=”address-block”>
+                <p>City – ${user.address.city}</p>
+                <p>Street – ${user.address.street}</p>
+                <p>Suite – ${user.address.suite}</p>
+                <p>Zip code – ${user.address.zipcode}</p>
+               </div>
+            </div>`
     )
 }
