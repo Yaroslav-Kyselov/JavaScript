10a1d7c5d222b8a6c81911319159e3262b9f6b9f
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-04-29 21:11:53 +0300

    js HW3 js + css

--- Lesson/HomeWork/HW-3/HW-3/main.js
+++ Lesson/HomeWork/HW-3/HW-3/main.js
@@ -1,3 +1,44 @@
+// За допомогою циклу вивести всю інформацію про користувачів, використовуючи шаблон
+//
+//
+//
+// <div class=”users-box”>
+//
+// <div class=”user-block”>
+//
+// <h2>ID – NAME – USERNAME </h2>
+//
+// <h3>EMAIL – PHONE</h3>
+//
+// <div class=”address-block”>
+//
+// <p>City – ADDRESS.CITY</p>
+//
+// <p>Street – ADDRESS.STREET</p>
+//
+// <p>Suite – ADDRESS.SUITE</p>
+//
+// <p>Zip code – ADDRESS.ZIPCODE</p>
+//
+// </div>
+//
+// </div>
+//
+//
+//
+// <!–
+//
+// …
+//
+// …
+//
+// …
+//
+//             other users
+//
+//             –>
+// за допомогою стилів, побудувати сітку по 2 об’єктах в рядок
+
 let usersList = [
     {
         id: 1,
@@ -230,3 +271,28 @@ let usersList = [
         }
     }
 ];
+for (const user of usersList) {
+    document.write(
+        `<div class=”users-box”>
+
+<div class=”user-block”>
+
+<h2>${user.id} – ${user.name} – ${user.username} </h2>
+
+    <h3>${user.email} – ${user.phone}</h3>
+
+    <div class=”address-block”>
+
+    <p>City – ${user.address.city}</p>
+
+    <p>Street – ${user.address.street}</p>
+
+    <p>Suite – ${user.address.suite}</p>
+
+    <p>Zip code – ${user.address.zipcode}</p>
+
+</div>
+
+</div>`
+    )
+}
