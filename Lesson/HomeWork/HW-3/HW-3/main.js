20644cac4a0bcc7909eb08532e19f3ff1efc79ea
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-04-30 20:18:28 +0300

    js HW3 js + css fin2

--- Lesson/HomeWork/HW-3/HW-3/main.js
+++ Lesson/HomeWork/HW-3/HW-3/main.js
@@ -277,12 +277,13 @@ for (const user of usersList) {
             <div class=user-block>
                 <h2>${user.id} – ${user.name} – ${user.username} </h2>
                 <h3>${user.email} – ${user.phone}</h3>
-            </div>
-            <div class=address-block>
-                <p>City – ${user.address.city}</p>
-                <p>Street – ${user.address.street}</p>
-                <p>Suite – ${user.address.suite}</p>
-                <p>Zip code – ${user.address.zipcode}</p>
+
+                <div class=address-block>
+                    <p>City – ${user.address.city}</p>
+                    <p>Street – ${user.address.street}</p>
+                    <p>Suite – ${user.address.suite}</p>
+                    <p>Zip code – ${user.address.zipcode}</p>
+                </div>
             </div>
         </div>`
     )
