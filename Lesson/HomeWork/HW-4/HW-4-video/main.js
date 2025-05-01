dc7d4b9c48c7de73c0094515f76c34092acbfb12
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-01 19:00:32 +0300

    js HW4 video

---
+++ Lesson/HomeWork/HW-4/HW-4-video/main.js
@@ -0,0 +1,27 @@
+
+// створити функцію яка обчислює та повертає площу прямокутника зі сторонами a b
+// function square(aNum, bNum) {
+//     const result=aNum*bNum;
+//     console.log(result);
+//     return result;
+// }
+// square(5,5)
+// square(22,33)
+
+// створити функцію яка обчислює та повертає площу кола з радіусом r
+// function circle(radius) {
+//     const result=Math.PI*radius*radius
+//     console.log(result);
+//     return result;
+// }
+// circle(24)
+
+// // створити функцію яка приймає масив та виводить кожен елемент
+// function foobar(array) {
+//     for (const item of array) {
+//         console.log(item);
+//     }
+//  }
+//  foobar([3,4,5,6,7,8,9,2,1,3,5])
+
+
