f3979864b42f736267fe87b8bbf0cf9b68b58b84
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-07 20:48:52 +0300

    js HW6

--- Lesson/HomeWork/HW-5/main.js
+++ Lesson/HomeWork/HW-5/main.js
@@ -38,3 +38,6 @@
 // Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 // let str = ‘Ревуть воли як ясла повні’;
 // let arr = stringToarray(str); [‘Ревуть’, ‘воли’, ‘як’, ‘ясла’, ‘повні’]
+let str ='Ревуть воли як ясла повні';
+let arr = stringToarray(str);
+    console.log(arr);
