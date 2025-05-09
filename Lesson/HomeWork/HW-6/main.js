39b3f047818eb364cc20e756ae2c4a07981e8199
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-09 18:06:52 +0300

    js HW6.1

--- Lesson/HomeWork/HW-6/main.js
+++ Lesson/HomeWork/HW-6/main.js
@@ -95,41 +95,91 @@
 // написати пошук всіх об’єктів, в яких в modules є sass
 // написати пошук всіх об’єктів, в яких в modules є docker
 
-let coursesArray = [
-    {
-        title: 'JavaScript Complex',
-        monthDuration: 5,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
-    },
-    {
-        title: 'Java Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
-    },
-    {
-        title: 'Python Complex',
-        monthDuration: 6,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
-    },
-    {
-        title: 'QA Complex',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
-    },
-    {
-        title: 'FullStack',
-        monthDuration: 7,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
-    },
-    {
-        title: 'Frontend',
-        monthDuration: 4,
-        hourDuration: 909,
-        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'git', 'java core', 'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular', 'aws', 'docker', 'python core', 'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js', 'python', 'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+// console.log(coursesArray.filter(course => {
+//     return course.modules.includes('sass');
+// }));
+//
+// console.log(coursesArray.filter(course => {
+//     return course.modules.includes('docker');
+// }));
+
+// описати колоду карт (від 6 до туза без джокерів). Більшу частину колоди можна описати з використанням циклу
+// Після опису, використовуючи функції масивів:
+//  – знайти піковий туз
+//  – всі шістки
+//  – всі червоні карти
+//  – всі буби
+//  – всі трефи від 9 та більше
+//
+// Приклад моделі об’єкту карти:
+// {
+//     cardSuit: ”, // ‘spade’, ‘diamond’,’heart’, ‘clubs’
+//     value: ”, // ‘6’-’10’, ‘ace’,’jack’,’queen’,’king’
+//     color:”, // ‘red’,’black’
+// }
+
+const suits = ['spade', 'diamond', 'heart', 'club'];
+const values = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+const cards = [];
+for (const suit of suits) {
+    for (const value of values) {
+        const card = {cardSuit: suit, value: value};
+        if (suit === 'heart' || suit === 'diamond') {
+            card.color = 'red';
+        } else {
+            card.color = 'black';
+        }
+        cards.push(card);
     }
-];
+}
+console.log(cards);
+// - знайти піковий туз
+console.log(cards.find(card => card.value === 'ace' && card.cardSuit === 'spade'));
+// - всі шістки
+console.log(cards.filter(card => card.value === '6'));
+// - всі червоні карти
+console.log(cards.filter(card => card.color === 'red'));
+
+// - всі буби
+console.log(cards.filter(card => card.cardSuit === 'diamond'));
+
+// - всі трефи від 9 та більше
+console.log(cards.filter(card => card.cardSuit === 'club' && (card.value !== '6' || card.value !== '7' || card.value !== '8' || card.value !== '9')));
