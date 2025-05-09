f7d2f3ae08d7d261b152bf6c15639067ec7a20ee
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-09 18:25:57 +0300

    js HW6 fin

--- Lesson/HomeWork/HW-6/main.js
+++ Lesson/HomeWork/HW-6/main.js
@@ -156,30 +156,30 @@
 //     color:”, // ‘red’,’black’
 // }
 
-const suits = ['spade', 'diamond', 'heart', 'club'];
-const values = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
-const cards = [];
-for (const suit of suits) {
-    for (const value of values) {
-        const card = {cardSuit: suit, value: value};
-        if (suit === 'heart' || suit === 'diamond') {
-            card.color = 'red';
-        } else {
-            card.color = 'black';
-        }
-        cards.push(card);
-    }
-}
-console.log(cards);
-// - знайти піковий туз
-console.log(cards.find(card => card.value === 'ace' && card.cardSuit === 'spade'));
-// - всі шістки
-console.log(cards.filter(card => card.value === '6'));
-// - всі червоні карти
-console.log(cards.filter(card => card.color === 'red'));
-
-// - всі буби
-console.log(cards.filter(card => card.cardSuit === 'diamond'));
+// const suits = ['spade', 'diamond', 'heart', 'club'];
+// const values = ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'];
+// const cards = [];
+// for (const suit of suits) {
+//     for (const value of values) {
+//         const card = {cardSuit: suit, value: value};
+//         if (suit === 'heart' || suit === 'diamond') {
+//             card.color = 'red';
+//         } else {
+//             card.color = 'black';
+//         }
+//         cards.push(card);
+//     }
+// }
+// console.log(cards);
+// // - знайти піковий туз
+// console.log(cards.find(card => card.value === 'ace' && card.cardSuit === 'spade'));
+// // - всі шістки
+// console.log(cards.filter(card => card.value === '6'));
+// // - всі червоні карти
+// console.log(cards.filter(card => card.color === 'red'));
+//
+// // - всі буби
+// console.log(cards.filter(card => card.cardSuit === 'diamond'));
 
 // - всі трефи від 9 та більше
-console.log(cards.filter(card => card.cardSuit === 'club' && (card.value !== '6' || card.value !== '7' || card.value !== '8' || card.value !== '9')));
+// console.log(cards.filter(card => card.cardSuit === 'club' && (card.value !== '6' && card.value !== '7' && card.value !== '8')));
