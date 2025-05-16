a296ab63ddb5cbb80136dc540ded2f0dceb9c9b8
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-16 19:58:02 +0300

    js HW7

--- Lesson/HomeWork/HW-7/main.js
+++ Lesson/HomeWork/HW-7/main.js
@@ -127,3 +127,36 @@
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+
+function Car(model, producer, year, maxSpeed, engineVolume) {
+    this.model = model;
+    this.producer=producer;
+    this.year=year;
+    this.maxSpeed=maxSpeed;
+    this.engineVolume=engineVolume;
+    this.drive=function () {
+        console.log(`їдемо зі швидкістю ${maxSpeed} на годину`);
+    };
+    this.info=function () {
+        for (const key in this) {
+            console.log(key, this[key]);
+        }
+    }
+    this.increaseMaxSpeed=function (speedToAdd) {
+        if (speedToAdd >0) this.maxSpeed = this.maxSpeed+speedToAdd;
+    }
+    this.changeYear = function (year) {
+        if (year > 1945) this.year = year;
+    };
+
+    this.addDriver = function (driver) {
+        if (driver) this.driver = driver;
+    };
+}
+const car=new Car('Ford','Edge',2018,180,2.4);
+car.drive();
+car.info();
+car.increaseMaxSpeed(100);
+car.changeYear(2000);
+car.addDriver({});
+console.log(car);
