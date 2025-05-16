391ea66df11fd906cc7f3ba1459356ebe53b621f
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-16 20:14:21 +0300

    js HW7

--- Lesson/HomeWork/HW-7/main.js
+++ Lesson/HomeWork/HW-7/main.js
@@ -168,39 +168,39 @@
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
-
-class Car {
-    constructor(model, producer, year, maxSpeed, engineVolume) {
-        this.mode = model;
-        this.producer = producer;
-        this.year = year;
-        this.maxSpeed = maxSpeed;
-        this.engineVolume = engineVolume;
-    }
-    drive() {
-        console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
-    };
-    info() {
-        for (const key in this) {
-            console.log(key, this[key]);
-        }
-    };
-    increaseMaxSpeed(speedToAdd) {
-        if (speedToAdd > 0) this.maxSpeed = this.maxSpeed + speedToAdd;
-    };
-    changeYear = function (year) {
-        if (year > 1945) this.year = year;
-    };
-    addDriver = function (driver) {
-        if (driver) this.driver = driver;
-    };
-
-}
-const car=new Car('Ford','Edge',2018,180,2.4);
-console.log(car);
-car.drive();
-car.info();
-car.increaseMaxSpeed(100);
-car.changeYear(2020);
-car.addDriver({});
-console.log(car);
+//
+// class Car {
+//     constructor(model, producer, year, maxSpeed, engineVolume) {
+//         this.mode = model;
+//         this.producer = producer;
+//         this.year = year;
+//         this.maxSpeed = maxSpeed;
+//         this.engineVolume = engineVolume;
+//     }
+//     drive() {
+//         console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)
+//     };
+//     info() {
+//         for (const key in this) {
+//             console.log(key, this[key]);
+//         }
+//     };
+//     increaseMaxSpeed(speedToAdd) {
+//         if (speedToAdd > 0) this.maxSpeed = this.maxSpeed + speedToAdd;
+//     };
+//     changeYear = function (year) {
+//         if (year > 1945) this.year = year;
+//     };
+//     addDriver = function (driver) {
+//         if (driver) this.driver = driver;
+//     };
+//
+// }
+// const car=new Car('Ford','Edge',2018,180,2.4);
+// console.log(car);
+// car.drive();
+// car.info();
+// car.increaseMaxSpeed(100);
+// car.changeYear(2020);
+// car.addDriver({});
+// console.log(car);
