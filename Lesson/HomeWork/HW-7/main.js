471d466087914c8b4fd5a880b78143b16279fdd0
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-05-16 19:28:28 +0300

    js HW7

--- Lesson/HomeWork/HW-7/main.js
+++ Lesson/HomeWork/HW-7/main.js
@@ -96,27 +96,34 @@
 
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
-function Client(id, name, surname , email, phone, order) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.phone = phone;
-        this.order = order;
-}
-const client = new Client(1,"Leanne Graham","Bret", "Sincere@april.biz","1-770-736-8031 x56442", ['tv', 'PS','dfg']);
+// function Client(id, name, surname , email, phone, order) {
+//         this.id = id;
+//         this.name = name;
+//         this.surname = surname;
+//         this.email = email;
+//         this.phone = phone;
+//         this.order = order;
+// }
+// const client = new Client(1,"Leanne Graham","Bret", "Sincere@april.biz","1-770-736-8031 x56442", ['tv', 'PS','dfg']);
+//
+// let clients =[
+//     new Client(1,"Leanne Graham","Bret", "Sincere@april.biz","1-770-736-8031 x56442", ['tv', 'PS','dfg']),
+//     new Client(2,"Ervin Howell","Antonette", "Shanna@melissa.tv","010-692-6593 x09125",['PS', 'milk', 'wdaad']),
+//     new Client(3,"Clementine Bauch","Samantha","Nathan@yesenia.net","1-463-123-4447",['dhfdg', 'sdfgfj', 'sgdg']),
+//     new Client(4,"Patricia Lebsack","Karianne","Julianne.OConner@kory.org","493-170-9623 x156",['dhfgjgjh', 'sdfgfdj', 'sgdg']),
+//     new Client(5,"Chelsey Dietrich","Kamren","Lucio_Hettinger@annie.ca","(254)954-1289",['dhdgjh', 'sdfj', 'sgdg', 'iuiuito', 'fjfkdlls']),
+//     new Client(6,"Mrs. Dennis Schulist","Leopoldo_Corkery","Karley_Dach@jasper.info","1-477-935-8478 x6430",['dhf', 'sdj', 'sgdg']),
+//     new Client(7,"Kurtis Weissnat","Elwyn.Skiles","Telly.Hoeger@billy.biz","210.067.6132",['dhf', 'sdfg', 'sgdg']),
+//     new Client(8,"Nicholas Runolfsdottir V","Maxime_Nienow","Sherwood@rosamond.me","586.493.6943 x140",['dhfdg', 'sdfgj', 'sgdg']),
+//     new Client(9,"Glenna Reichert","Delphine","Chaim_McDermott@dana.io","(775)976-6794 x41206",['gjgjh', 'sdfj', 'sgdg']),
+//     new Client(10,"Clementina DuBuque","Moriah.Stanton","Rey.Padberg@karina.biz","024-648-3804",['dhdgjh'])
+// ];
+// const sort = clients.sort((a, b) => a.order.length - b.order.length);
+// console.log(sort);
 
-let clients =[
-    new Client(1,"Leanne Graham","Bret", "Sincere@april.biz","1-770-736-8031 x56442", ['tv', 'PS','dfg']),
-    new Client(2,"Ervin Howell","Antonette", "Shanna@melissa.tv","010-692-6593 x09125",['PS', 'milk', 'wdaad']),
-    new Client(3,"Clementine Bauch","Samantha","Nathan@yesenia.net","1-463-123-4447",['dhfdg', 'sdfgfj', 'sgdg']),
-    new Client(4,"Patricia Lebsack","Karianne","Julianne.OConner@kory.org","493-170-9623 x156",['dhfgjgjh', 'sdfgfdj', 'sgdg']),
-    new Client(5,"Chelsey Dietrich","Kamren","Lucio_Hettinger@annie.ca","(254)954-1289",['dhdgjh', 'sdfj', 'sgdg', 'iuiuito', 'fjfkdlls']),
-    new Client(6,"Mrs. Dennis Schulist","Leopoldo_Corkery","Karley_Dach@jasper.info","1-477-935-8478 x6430",['dhf', 'sdj', 'sgdg']),
-    new Client(7,"Kurtis Weissnat","Elwyn.Skiles","Telly.Hoeger@billy.biz","210.067.6132",['dhf', 'sdfg', 'sgdg']),
-    new Client(8,"Nicholas Runolfsdottir V","Maxime_Nienow","Sherwood@rosamond.me","586.493.6943 x140",['dhfdg', 'sdfgj', 'sgdg']),
-    new Client(9,"Glenna Reichert","Delphine","Chaim_McDermott@dana.io","(775)976-6794 x41206",['gjgjh', 'sdfj', 'sgdg']),
-    new Client(10,"Clementina DuBuque","Moriah.Stanton","Rey.Padberg@karina.biz","024-648-3804",['dhdgjh'])
-];
-const sort = clients.sort((a, b) => a.order.length - b.order.length);
-console.log(sort);
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску, максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+// -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+// -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+// -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+// -- changeYear (newValue) - змінює рік випуску на значення newValue
+// -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
