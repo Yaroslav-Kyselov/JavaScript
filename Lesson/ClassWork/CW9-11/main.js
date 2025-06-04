ad8d798e6fa546b986482d486b0f6a99a766235b
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-06-04 11:06:25 +0300

    repeat

--- Lesson/ClassWork/CW9-11/main.js
+++ Lesson/ClassWork/CW9-11/main.js
@@ -1,7 +1,38 @@
 // DOM
-
 // console.log(document.head);
+// console.log(document.head.innerHTML);
 // console.log(document.head.children);
-// console.log(document.body);
-let ul2=document.getElementById('list-2');
-console.log(ul2);
+// console.log(document.body.innerHTML);
+
+// ID
+// let ul1 = document.getElementById('list-1');
+// console.log(ul1);
+
+// class
+// let menuCollection = document.getElementsByClassName('menu');
+// console.log(menuCollection);
+//
+// for (const menuCollectionElement of menuCollection) {
+//     console.log(menuCollectionElement);
+// }
+
+// TAG
+// let liList = document.getElementsByTagName('li');
+// console.log(liList);
+
+// let list1 = document.getElementById('list-1');
+// console.log(list1);
+//
+// let list1LiCollection = list1.getElementsByTagName('li');
+// console.log(list1LiCollection);
+
+
+// console.log(document.querySelector('body .menu'));
+// console.log(document.querySelectorAll('body .menu'));
+
+// let nodeListOf = document.querySelectorAll('#list-2 > li');
+// console.log(nodeListOf);
+//
+// for (const node of nodeListOf) {
+//     console.log(node);
+// }
