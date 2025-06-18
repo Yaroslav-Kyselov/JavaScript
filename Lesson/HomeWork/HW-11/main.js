661592a442325e51612a086874f7ce1b7afa6498
Yaroslav-Kyselov <mmmyaroslav@gmail,com>
2025-06-18 21:30:41 +0300

    HW10 fin

---
+++ Lesson/HomeWork/HW-11/main.js
@@ -0,0 +1,8 @@
+// #HmvAfRQM
+// - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+
+fetch('https://dummyjson.com/carts')
+    .then(value => value.json())
+    .then(cartsObject => {
+        console.log(value)
+    });
